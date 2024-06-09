54ddc641f6404660b0287e52f9c71a868c1b4473
Justin <jul155@pitt.edu>
2024-06-09 06:42:05 -0400

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+
+void Motor_Init() {
+
+}
+
+void Motor_Stop() {
+
+}
+
+void Motor_Forward() {
+
+}
+
+void Motor_Backward() {
+
+}
+
+void Motor_Left() {
+
+}
+
+void Motor_Right() {
+
+}
+
+int main(void) {
+  // put your setup code here, to run once:
+  PORTB |= 0x02;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
