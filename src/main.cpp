345776219ad4dd6ea82284048fbae4402e76d67f
Justin <jul155@pitt.edu>
2024-06-09 06:50:38 -0400

    Began prototyping motor code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
 
+void PWM_Init(int d) {
+
+}
+
 void Motor_Init() {
 
+  PWM_Init(14999);
+
+  // Motor Ports
+  DDRB |= 0x10; // sets PB4 as output (Sleep Pin) 
+  PORTB |= 0x10; // sets PB4 low
+
+  // Input Pins for the DRV8833
+  DDRH |= 0x78; // sets as input
 }
 
 void Motor_Stop() {
@@ -26,7 +38,6 @@ void Motor_Right() {
 
 int main(void) {
   // put your setup code here, to run once:
-  PORTB |= 0x02;
 }
 
 void loop() {
