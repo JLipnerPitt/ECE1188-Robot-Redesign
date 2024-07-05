f0a09243b4af15c7acdaab977c9f4b6211d3202c
Justin Lipner <jul155@pitt.edu>
2024-07-05 07:18:08 -0400

    First commit to LFR branch.

--- src/main.c
+++ src/main.c
@@ -2,12 +2,14 @@
 #include "PWM.h"
 #include "Motor.h"
 
+void PWM_Init(int d) {
+
+}
+
 int main(void) {
-  Motor_Init(16000);
-  //PWM_Init(15999);
-  //PWM_Init2(15999);
-  while (1) {
-    //PWM_Signal(8000);
-    Motor_Forward(8000,8000);
-  }
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
 }
